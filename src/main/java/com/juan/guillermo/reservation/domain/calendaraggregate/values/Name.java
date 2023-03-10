16937e028c6a73df214c52be76ba6f7f8b676639
Juangmcz <juangui-089@hotmail.com>
2023-03-10 14:40:33 -0500

    Regex validations were added.

--- src/main/java/com/juan/guillermo/reservation/domain/calendaraggregate/values/Name.java
+++ src/main/java/com/juan/guillermo/reservation/domain/calendaraggregate/values/Name.java
@@ -2,12 +2,24 @@ package com.juan.guillermo.reservation.domain.calendaraggregate.values;
 
 import com.juan.guillermo.reservation.generic.ValueObject;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Name implements ValueObject<String> {
 
     private final String value;
+    private static final String NAME_REGEX = "^[a-zA-Z\\s']+$";
 
     public Name(String value) {
-        this.value = value;
+
+        Pattern pattern = Pattern.compile(NAME_REGEX);
+        Matcher matcher = pattern.matcher(value);
+
+        if(matcher.matches()){
+            this.value = value;
+        }else{
+            throw new IllegalArgumentException("Please, enter a valid name");
+        }
     }
 
     @Override
--- src/main/java/com/juan/guillermo/reservation/domain/reservationaggregate/values/Cell.java
+++ src/main/java/com/juan/guillermo/reservation/domain/reservationaggregate/values/Cell.java
@@ -2,12 +2,24 @@ package com.juan.guillermo.reservation.domain.reservationaggregate.values;
 
 import com.juan.guillermo.reservation.generic.ValueObject;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Cell implements ValueObject<String> {
 
     private final String value;
+    private static final String CELL_REGEX = "^\\d{10}$";
 
     public Cell(String value) {
-        this.value = value;
+
+        Pattern pattern = Pattern.compile(CELL_REGEX);
+        Matcher matcher = pattern.matcher(value);
+
+        if(matcher.matches()){
+            this.value = value;
+        }else{
+            throw new IllegalArgumentException("Please, enter a valid Cellphone number");
+        }
     }
 
     @Override
